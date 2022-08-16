a55d34b8c263b666903fe0eaba94750fb1b7d2f8
Alex Geis <siegxela@gmail.com>
2022-08-16 08:45:26 -0600

    add breadth-first search example

--- js/depth-first-search.js
+++ js/depth-first-search.js
@@ -9,6 +9,45 @@ Following is how a DFS works −
 3) Repeat Rule 1 and Rule 2 until the stack is empty.
 */
 
+/**
+ * Implementation of DFS (depth-first search) Algorithm to find the shortest path from a start to a target node.
+ * Given a start node, this returns the node in the tree below the start node with the target value (or null if it doesn't exist)
+ * Runs in O(n), where n is the number of nodes in the tree, or O(b^d), where b is the branching factor and d is the depth.
+ *
+ * @param start  the node to start the search from
+ * @param target the value to search for
+ * @return The node containing the target value or null if it doesn't exist.
+ */
+const dfs = function (start, target) {
+	console.log("Visiting Node " + start.value);
+	if (start.value === target) {
+		// We have found the goal node we we're searching for
+		console.log("Target node found");
+		return start;
+	}
+
+	// Recurse with all children
+	for (var i = 0; i < start.children.length; i++) {
+		var result = dfs(start.children[i], target);
+		if (result != null) {
+			// We've found the goal node while traversing down that child
+			return result;
+		}
+	}
+
+	// We've gone through all children and not found the goal node
+	console.log(
+		"Went through all children of " +
+			start.value +
+			", returning to it's parent."
+	);
+	return null;
+};
+
+module.exports = { dfs };
+
+/*
+// // alt solution
 function DFS(node) {
 	// Create a Stack and add our initial node in it
 	let stackDFS = new Stack(this.nodes.length);
@@ -64,3 +103,4 @@ g.DFS("A");
 // B
 // G
 // C
+*/
