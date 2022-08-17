438ae79b03fe0588ed7741f874fc01704385f924
Alex Geis <siegxela@gmail.com>
2022-08-17 16:42:31 -0600

    add test script and clean up comments - linked list module complete

--- js/linked-list.js
+++ js/linked-list.js
@@ -138,4 +138,37 @@ const LinkedList = () => {
 		nodeBeforeIndex.nextNode = nodeBeforeIndex.nextNode.nextNode;
 		size--;
 	};
+	return {
+		append,
+		prepend,
+		getSize,
+		getHead,
+		at,
+		tail,
+		pop,
+		contains,
+		find,
+		toString,
+		insertAt,
+		removeAt,
+	};
 };
+
+// Test Script
+const list = LinkedList();
+list.append(5);
+list.append(6);
+list.append(7);
+list.prepend(10);
+list.prepend(9);
+list.prepend(8);
+list.insertAt(3, 1);
+list.pop();
+list.removeAt(4);
+
+console.log(list.size);
+console.log(list.head);
+console.log(list.at(1));
+console.log(list.find(10));
+console.log(list.tail());
+console.log(list.toString());
